cc181b1b811d94a6fa5818dc3a6e0f279417ec9d
Kacper Wojak <k.wojak@medicalgorithmics.com>
2022-12-17 12:14:51 +0100

    add absolute imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import { Inter } from '@next/font/google'
 
-import '../styles/globals.css'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Esas from './esas'
 import '../styles/globals.css'
 
 export default function HomePage() {
   return (
     <div>
-      <Esas a={2} b={1} />
       <h1 className="text-6xl font-bold underline">Hello world!</h1>
     </div>
   )
